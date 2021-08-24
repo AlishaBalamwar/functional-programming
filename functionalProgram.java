1d5c0f068f12c33f808b55c0d84584407b36324a
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-24 15:03:36 +0530

    computing distance from origin

--- functionalProgram.java
+++ functionalProgram.java
@@ -1,44 +1,22 @@
 package funcional;
 import java.util.Scanner;
-import java.util.Arrays;
 class functionalProgram{
-    
-    static void findTriplets(int[] arr, int n) {
-        boolean found = false;
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[i] + arr[j] + arr[k] == 0) {
-                        System.out.print(arr[i]);
-                        System.out.print(" ");
-                        System.out.print(arr[j]);
-                        System.out.print(" ");
-                        System.out.print(arr[k]);
-                        System.out.print("\n");
-                        found = true;
-                    }
-                }
-            }
-        }
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("COMPUTE DISTANCE FROM ORIGIN");
+	        System.out.println("ENTER COORDINATES  X & Y");
+	        // input point coordinates
+	        int x = sc.nextInt();
+	        int y = sc.nextInt();
 
-      
-        if (!found)
-            System.out.println(" not exist ");
 
-    }
+	        
+	        double distance = Math.sqrt(
+	                Math.pow(x, 2) + Math.pow(y, 2));
 
-	 public static void main(String[] args)
-	    {
-		 Scanner sc = new Scanner(System.in);
-	        int[] arr= new int[9];
-	        System.out.print("enter value for element insert");
-	        int a = sc.nextInt();
+	        System.out.println("Distance from (0, 0) to (" +
+	                x + ", " + y + ") is " +
+	                distance);
 
-	        for (int l = 0; l < a; l++) {
-	            arr[l] = sc.nextInt();
-	        }
-	        System.out.println("Given array is: " + Arrays.toString(arr));
-	        int n = arr.length;
-	        findTriplets(arr, n);
-	    }
-}
+	    } 
+   }
