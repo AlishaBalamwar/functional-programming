5ed2c6e3bc8e122a7c252421b8818de718c9f235
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-24 15:06:53 +0530

    computation of quadratic equation

--- functionalProgram.java
+++ functionalProgram.java
@@ -1,22 +1,51 @@
 package funcional;
 import java.util.Scanner;
 class functionalProgram{
-	 public static void main(String[] args) {
+	  public static void main(String[] args) {
 	        Scanner sc = new Scanner(System.in);
-	        System.out.println("COMPUTE DISTANCE FROM ORIGIN");
-	        System.out.println("ENTER COORDINATES  X & Y");
-	        // input point coordinates
-	        int x = sc.nextInt();
-	        int y = sc.nextInt();
+	        System.out.println("WELCOME TO COMPUTATION OF QUADRATIC EQUATION");
+	        System.out.println("          Ax*x + Bx + C      ");
+	        System.out.println();
+	        System.out.println("Enter value for A ,B ,C ");
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int c = sc.nextInt();
+	        System.out.println();
+	        System.out.println("Quadratic Equation is:  "+a+"xx + "+b+"x + "+c);
+	        double root1, root2;
 
+	        // calculate the determinant (b2 - 4ac)
+	        double determinant = (b * b) - (4 * a * c);
 
-	        
-	        double distance = Math.sqrt(
-	                Math.pow(x, 2) + Math.pow(y, 2));
+	        // check if determinant is greater than 0
+	        if (determinant > 0) {
 
-	        System.out.println("Distance from (0, 0) to (" +
-	                x + ", " + y + ") is " +
-	                distance);
+	            // two real and distinct roots
+	            root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+	            root2 = (-b - Math.sqrt(determinant)) / (2 * a);
 
-	    } 
+	            System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
+	        }
+
+	        // check if determinant is equal to 0
+	        else if (determinant == 0) {
+
+	            // two real and equal roots
+	            // determinant is equal to 0
+	            // so -b + 0 == -b
+	            root1 = root2 = (float)-b / (2 * a);
+	            System.out.format("root1 = root2 = %.2f;", root1);
+	        }
+
+	        // if determinant is less than zero
+	        else {
+
+	            // roots are complex number and distinct
+	            double real = -b / (2 * a);
+	            double imaginary = Math.sqrt(-determinant) / (2 * a);
+	            System.out.format("root1 = %.2f+%.2fi", real, imaginary);
+	            System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
+	        }
+	    }
+
    }
